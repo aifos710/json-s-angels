499e5666d0befddff33655fa6e343c36fb6518d1
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-11-08 15:47:30 -0500

    añadiendo js

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
-
+var nombre= localStorage.getItem("nombre");
+var src= localStorage.getItem("src");
+var cargar= function(){
 	$("#nombre").focus();
 	$("#nombre").keydown(validarNombreAp);
 	$("#next3").click(validarDatos);
-
+	$("#perfil-menu").click(menu);
+	$(".absolute").click(desaparecerMenu);
+	if((src!=null || src!=undefined)&& location.href.includes("buscador.html")){
+		$("#logo-usuario").attr("src", src);
+		$("#user").attr("src",src);
+	}
 	function validarNombreAp(evento){
 		var ascii = evento.keyCode;
 		if (ascii == 8 || ascii == 32 || (ascii >= 65 && ascii <= 90 )|| (ascii >= 97 && ascii <= 122 )) {
@@ -12,7 +18,6 @@ $(document).ready(function(){
 			return false;
 		}
 	}
-
 	function validarDatos(){
 		var email = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 		var dni = $(".dni").val().length;
@@ -25,7 +30,6 @@ $(document).ready(function(){
 			alert("Por favor, llena tus datos")
 		}
 	}
-
 	var app_id = '1850360375199136';
 	var scopes = '';
 
@@ -53,6 +57,21 @@ $(document).ready(function(){
 		});
 	};
 
+
+
+	$(document).on('click', '#login', function(e) {
+		e.preventDefault();
+		facebookLogin();
+	});
+	$(document).on('click', '#logout', function(e) {
+		e.preventDefault();
+		if (confirm("¿Está seguro?"))
+			facebookLogout();
+		else
+			return false;
+	});
+}
+$(document).ready(cargar);
 	var statusChangeCallback = function(response, callback) {
 		console.log(response);
 
@@ -65,22 +84,18 @@ $(document).ready(function(){
 			callback(false);
 		}
 	}
-
 	var checkLoginState = function(callback) {
 		FB.getLoginStatus(function(response) {
 			callback(response);
 		});
 	}
-
 	var getFacebookData =  function() {
 		FB.api('/me', function(response) {
-			$('#login').after(div_session);
-			$('#login').remove();
-			$('#facebook-session strong').text("Bienvenido: "+response.name);
-			$('#facebook-session img').attr('src','http://graph.facebook.com/'+response.id+'/picture?type=large');
+			localStorage.setItem("nombre", response.name);
+			localStorage.setItem("src", 'http://graph.facebook.com/'+response.id+'/picture?type=large');
+			window.location = 'buscador.html';
 		});
 	}
-
 	var facebookLogin = function() {
 		checkLoginState(function(data) {
 			if (data.status !== 'connected') {
@@ -101,15 +116,11 @@ $(document).ready(function(){
 			}
 		});
 	}
-	$(document).on('click', '#login', function(e) {
-		e.preventDefault();
-		facebookLogin();
-	});
-	$(document).on('click', '#logout', function(e) {
-		e.preventDefault();
-		if (confirm("¿Está seguro?"))
-			facebookLogout();
-		else
-			return false;
-	});
-})
+	var menu= function(){
+		$("#menu").animate({width:'toggle'},350);
+		$(".absolute").show();
+	};
+	var desaparecerMenu= function(){
+		$("#menu").animate({width:'toggle'},350);
+		$(".absolute").hide();
+	}
